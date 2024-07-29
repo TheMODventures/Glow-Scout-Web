dbe51b5118f196499a42f9738bce1ab8cfff7999
muhammd-amir-2535001 <lalkhan2636277@gmail.com>
2024-07-29 08:42:45 -0700

    rounded scout

--- src/components/helper/homeComponet/ScotWorkSection.jsx
+++ src/components/helper/homeComponet/ScotWorkSection.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const ScotWorkSection = () => {
   return (
-    <div className=" bg-[#FEF5E3] text-center py-20 rounded-t-[30px] border font-raleway">
+    <div className=" bg-[#FEF5E3] text-center py-20 rounded-t-[50px] border font-raleway">
       <Container>
         <h2 className="text-5xl md:text-4xl mb-8 mt-8 font-raleway font-thin text-darkMahron">
           How Glow Scout Works
